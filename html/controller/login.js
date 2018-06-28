228b3dface6234623db43176e4dd277aa1aaf716
VIVIES Antoine <antoine.vivies@epitech.eu>
2018-06-28 16:22:33 +0200

    front

--- html/controller/login.js
+++ html/controller/login.js
@@ -1,6 +1,12 @@
 quizzUpController.controller('login', ['$scope', '$location', '$rootScope', '$route', '$http', function($scope, $location, $rootScope, $route, $http) {
     $scope.get_ready = function(){
-/*         $http.post('/someUrl', data, config).then(successCallback, errorCallback);
- */        $location.path('/ready')
+        var teamName = angular.element("#teamName")[0].value
+
+        if (teamName == '' || teamName == undefined) {
+            alert('Empty team name')
+            return;
+        }
+/*      $http.post('/someUrl', data, config).then(successCallback, errorCallback);
+*/      $location.path('/ready')
     }
 }]);
--- html/controller/ready.js
+++ html/controller/ready.js
@@ -2,4 +2,15 @@ quizzUpController.controller('ready', ['$scope', '$location', '$rootScope', '$ro
     $scope.get_all_team_connected = function(){
         
     }
+    $scope.getReady = function () {
+        var button = $("#readyButton")
+
+        if (button.text() == "Ready") {
+            button.attr("class", "uk-button uk-button-danger uk-align-center")
+            button.text("Not Ready") 
+        } else {
+            button.attr("class", "uk-button uk-label-success uk-align-center")
+            button.text("Ready")
+        }
+    }
 }]);
